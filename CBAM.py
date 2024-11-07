6e9f75427e122cead77a793066ff461a4a97866a
hutianhan <45224688+hutianhan@users.noreply.github.com>
2024-11-07 09:08:50 +0800

    Update CBAM.py

--- CBAM.py
+++ CBAM.py
@@ -1,7 +1,5 @@
 
-# ------------------#
-# ResBlock+CBAM
-# ------------------#
+
 import torch
 import torch.nn as nn
 import torchvision
@@ -90,84 +88,3 @@ class SE_Block(nn.Module):                         # Squeeze-and-Excitation bloc
         x = self.conv2(x)
         out = self.sigmoid(x)*y
         return out
-
-
-if __name__=="__main__":
-    m1 = CBAM_Spatial()
-    m2 = CBAM_channel(channel=32)
-    m3=   SE_Block(in_planes=32)
-    m4=CBAM(channel=32)
-    input = torch.randn(1, 32, 64, 64)
-    out1 = m1(input)
-    out2 = m2(input)
-    out3=  m3(input)
-    out4 = m4(input)
-    print(out1.shape,out2.shape,out3.shape,out4.shape)
-
-'''
-class css(nn.Module):
-    def __init__(self,):
-        super(css, self).__init__()
-        self.l=nn.LeakyReLU()
-        #self.l = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-    def forward(self, x):
-        k=self.l(x)
-        print(k)
-        k=self.sigmoid(x)
-        return k
-
-x=torch.randn(2,1,2,2)
-net=css()
-y=net(x)
-print(x)
-print(y)
-'''
-
-
-'''
-class ResBlock_CBAM(nn.Module):
-    def __init__(self,in_places, places, stride=1,downsampling=False, expansion = 1):
-        super(ResBlock_CBAM,self).__init__()
-        self.expansion = expansion
-        self.downsampling = downsampling
-
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(in_channels=in_places,out_channels=places,kernel_size=1,stride=1, bias=False),
-            nn.BatchNorm2d(places),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=places, out_channels=places, kernel_size=3, stride=stride, padding=1, bias=False),
-            nn.BatchNorm2d(places),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=places, out_channels=places*self.expansion, kernel_size=1, stride=1, bias=False),
-            nn.BatchNorm2d(places*self.expansion),
-        )
-        self.cbam = CBAM(channel=places*self.expansion)
-
-        if self.downsampling:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels=in_places, out_channels=places*self.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(places*self.expansion)
-            )
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        residual = x
-        out = self.bottleneck(x)
-        print(x.shape)
-        out = self.cbam(out)
-        if self.downsampling:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-        return out
-
-
-model = ResBlock_CBAM(in_places=32, places=32)
-print(model)
-
-input = torch.randn(1, 32, 64, 64)
-out = model(input)
-print(out.shape)
-'''
